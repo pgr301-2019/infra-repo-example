6085c8df4953f677fce13436e169dfa817512740
glennbech <glenn.bech@gmail.com>
2019-10-12 23:48:13 +0200

    rename

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+
+cd infra/terraform
 wget https://releases.hashicorp.com/terraform/0.12.10/terraform_0.12.10_linux_amd64.zip
 unzip terraform_0.12.10_linux_amd64.zip
 chmod +x terraform
 
-cd infra/terraform
 ./terraform init
 ./terraform validate
 
