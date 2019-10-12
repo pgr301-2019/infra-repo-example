b09f5aa7b9cf6bae7fad2a49553a060d5f19eadf
glennbech <glenn.bech@gmail.com>
2019-10-12 22:59:51 +0200

    builders

---
+++ build.sh
@@ -0,0 +1,11 @@
+wget https://releases.hashicorp.com/terraform/0.12.10/terraform_0.12.10_linux_amd64.zip
+umzip terraform_0.12.10_linux_amd64.zip
+chmod +x terraform
+
+./terraform init
+./terraform validate
+
+if [[ $TRAVIS_BRANCH == 'master' ]]
+then
+    ./terraform apply -auto-approve
+fi
