379065b8dbfc4f7a12050cb598f22c1c4c66a69e
glennbech <glenn.bech@gmail.com>
2019-10-12 22:53:33 +0200

    initial

---
+++ infra/terraform/provider.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "pgr301bucket"
+    key    = "glenn.bech/terraform.tfstate"
+    region = "eu-north-1"
+  }
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "app_prefix" {
+  default = "pgr301-solution"
+}
+
+variable "pipeline_name" {
+  default = "pgr301-solution"
+}
+
