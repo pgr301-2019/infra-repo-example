92b3cdfe3fea6ca2caebf105986ad0e631dd1417
glennbech <glenn.bech@gmail.com>
2019-10-14 06:19:15 +0200

    duplicates

--- monitoring.tf
+++ monitoring.tf
@@ -2,7 +2,7 @@ resource "statuscake_test" "ci" {
   website_name = "app-ci"
   website_url = "${heroku_app.ci.web_url}"
   test_type = "HTTP"
-  check_rate = 300
+  check_rate = "300"
   contact_group = ["Default Contact Group"]
 }
 
@@ -10,7 +10,7 @@ resource "statuscake_test" "stage" {
   website_name = "app-stage"
   website_url = "${heroku_app.staging.web_url}"
   test_type = "HTTP"
-  check_rate = 300
+  check_rate = "300"
   contact_group = ["Default Contact Group"]
 
 }
@@ -19,7 +19,7 @@ resource "statuscake_test" "prod" {
   website_name = "app-prod"
   website_url = "${heroku_app.production.web_url}"
   test_type = "HTTP"
-  check_rate = 300
+  check_rate = "300"
   contact_group = ["Default Contact Group"]
 
 }
